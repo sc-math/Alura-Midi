c25eeb7360a5f0926a86bb106fb91a4631b1f3de
Matheus Silva <scruz.math@gmail.com>
2023-04-04 20:29:19 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom(seletorAudio){
+
+    const elemento = document.querySelector(seletorAudio);
+    
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+
+    else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    tecla = listaDeTeclas[contador];
+
+    const idAudio = '#som_' + tecla.classList[1];
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(evento){
+        
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.remove('ativa');
+        }
+    }
+}
